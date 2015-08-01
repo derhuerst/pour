968fa4c5db8c3c28437a664bee67272d7d63536c
Jannis R <mail@jannisr.de>
2015-08-01 05:42:05 +0200

    `demo`: object-object inheritance

--- demo/rendering.js
+++ demo/rendering.js
@@ -1,18 +1,18 @@
-pour.Particle.prototype.draw = function (context) {
+pour.Particle.draw = function (context) {
 	context.fillStyle = 'blue';
 	context.fillRect(this.position.x - 1|0, this.position.y - 1|0, 2, 2);
 };
-pour.Emitter.prototype.draw = function (context) {
+pour.Emitter.draw = function (context) {
 	context.fillStyle = 'red';
 	context.fillRect(this.position.x - 2|0, this.position.y - 2|0, 4, 4);
 };
-pour.Field.prototype.draw = function (context) {
+pour.Field.draw = function (context) {
 	context.strokeStyle = this.mass > 0 ? 'green' : 'red';
 	context.beginPath();
 	context.arc(this.position.x|0, this.position.y|0, Math.abs(Math.ceil(this.mass / 7)), 0, Math.PI * 2);
 	context.stroke();
 };
-pour.System.prototype.draw = function (context, i, length) {
+pour.System.draw = function (context, i, length) {
 	context.clearRect(0, 0, canvas.element.width, canvas.element.height);
 	for (i = 0, length = this.fields.length; i < length; i++) {
 		this.fields[i].draw(context);
